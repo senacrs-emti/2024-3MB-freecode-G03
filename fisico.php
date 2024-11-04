53ad0afd15df52dc11b9a9d4e9c44e781b9f4e7f
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-11-04 09:24:23 -0300

    fisico quase pronto

--- fisico.php
+++ fisico.php
@@ -1,93 +1,54 @@
 <!DOCTYPE html>
-<html lang="PT-BR">
-
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Bem Estar</title>
+    <title>Menu de Opções</title>
+   <link rel="stylesheet" href="fisico.css">
+   <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-  <?php
-  include_once "header.php";
-  ?>
+    <?php include_once "header.php"?>
+    <div class="container">
+        <div class="menu">
+           
+            <h2>Selecione uma opção:</h2>
+            <div class="menu-item imc">
+            <a href="imc.php">
+            <span class="card-text" style="font-size: 45px;">IMC</span>
+            <span class="card-subtext">Cálculo de IMC</span>
+            </a>
+        </div>
+        <div class="menu-item verificador">
+            <a href="verificador.php">
+            <span><img style="width: 100px; height: 100px;" src="img/halter.png" alt="Ícone de movimento"></span>
+            <span class="card-text">Verificador de movimento</span>
+            </a>
+        </div>
+        <div class="menu-item treino">
+            <a href="treino.php">
+            <span><img style="width: 100px; height: 100px;" src="img/braco.png" alt="Ícone de treino"></span>
+            <span class="card-text">Gerador de Treino</span>
+            </a>
+        </div>
+        <div class="menu-item avaliador">
+            <a href="avaliador.php">
+        <span><img style="width: 100px; height: 100px;" src="img/shape.png" alt="Ícone de avaliador"></span>
+        <span class="card-text">Avaliador de Físico</span>
+        </a>
+        </div>
 
-
-<main class="home_fisico_main">
-   <h1>
-    Selecione uma opção:
-   </h1>
-   <nav>
-    <ul>
-        <a href="#">
-            <li class="card_fisico">
-                <section>
-                    <h1>IMC</h1>
-                </section>
-                <h2>
-                    Cálculo IMC
-                </h2>
-        </li>
-    </a>
-    <a href="#">
-            <li class="card_fisico">
-              <section>
-                <img src="#" alt="">
-              </section>
-                <h2>
-                    Verificador de <br> Movimento
-                </h2>
-        </li>
-    </a>
-    <a href="#">
-            <li class="card_fisico">
-                <section>
-                    <img src="" alt="">
-                </section>
-                <h2>
-                    Gerador de treino
-                </h2>
-        </li>
-    </a>
-    <a href="#">
-            <li class="card_fisico">
-                <section>
-                    <img src="#" alt="">
-                </section>
-                <h2>
-                    Avaliador Físico
-                </h2>
-        </li>
-    </a>
-    </ul>
-   </nav>
-
-   <section>
-    <h1>
-    Motivos pelo qual o execício físico é importante:
-    </h1>
-    <nav>
-        <ul>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-            <li>lorem</li>
-        </ul>
-    </nav>
-   </section>
-</main>
-
-<?php
-include_once "footer.php"
-?>
-
-
-
-
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        </div>
+        <div class="info-box">
+            <h3>Motivos pelo qual o exercício físico é importante:</h3>
+            <ul>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+            </ul>
+        </div>
+    </div>
+    <?php include_once "footer.php"?>
 </body>
-
 </html>
