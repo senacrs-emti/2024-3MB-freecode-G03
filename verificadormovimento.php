31da0f77336989bce9a64911351e41b3a842f2fe
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-12-03 22:37:00 -0300

    verificador_movimento_pronto

--- verificadormovimento.php
+++ verificadormovimento.php
@@ -4,97 +4,98 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="static/css/verificadormovimento.css">
 </head>
 <body>
     <?php include_once "header.php"; ?>
         <main>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/06301301-Mountain-Climber_Cardio_360-logo.gif" alt="">
+                <h1>Mountain Climber Cardio</h1>
+            </section>
+            <section class="exercicio">
+                <img src="GIF/46461301-abdominal-obliquo-rotacao-russa-de-quadri.gif" alt="">
+                <h1>Abdominal Obliquo Rotacao Russa de Quadri</h1>
             </section>
             <section class="exercicio">
                 <img src="GIF/abdominal.gif" alt="">
                 <h1>Abdominal</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/agachamento-com-salto-tradicional.gif" alt="">
+                <h1>Agachamento Com Salto Tradicional</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/agachamento-livre-1.gif" alt="">
+                <h1>Agachamento Livre</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/barbell-lying-triceps-extension-skull-crusher.gif" alt="">
+                <h1>Barbell Lying Triceps Extension Skull Crusher</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/burpee.gif" alt="">
+                <h1>Burpee</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/costas-remada-no-smith-com-pegada-invertida.gif" alt="">
+                <h1>Costas Remada No Smith Com Pegada Invertida</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/desenvolvimento-para-ombros-com-halteres.gif" alt="">
+                <h1>Desenvolvimento Para Ombros Com Halteres</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/Elevacao-de-panturrilha-com-carga-em-uma-perna.gif" alt="">
+                <h1>Elevacao de Panturrilha Com Carga Em Uma Perna</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/flexao-de-bracos.gif" alt="">
+                <h1>Flexao de Bracos</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/levantamento-terra-deadlift-stiff-com-halteres-1.gif" alt="">
+                <h1>Levantamento Terra Deadlift Stiff com Halteres</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/ombros-elevacao-lateral-de-ombros-com-halteres.gif" alt="">
+                <h1>Ombros Elevacao Lateral de Ombros Com Halteres</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/pernas-afundo-tradicional-sem-pesos.gif" alt="">
+                <h1>Pernas Afundo Tradicional Sem Pesos</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/pernas-e-costas-levantamento-terra-deadlift.gif" alt="">
+                <h1>Pernas e Costas Levantamento Terra Deadlift</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/pernas-extensao-de-pernas-na-maquina.gif" alt="">
+                <h1>Pernas Extensao de Pernas na Maquina</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/pernas-leg-press-45-tradicional.gif" alt="">
+                <h1>Pernas Leg Press 45</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/Prancha.png" alt="" width="360px">
+                <h1>Prancha</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/remada-sentado-com-cabos-e-triangulo-para-costas.gif" alt="">
+                <h1>Remada Sentado com Cabos e Triangulo para Costas</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/rosca-biceps-martelo-com-halteres.gif" alt="">
+                <h1>Rosca Biceps Martelo com Halteres</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/supino-inclinado-com-halteres.gif" alt="">
+                <h1>Supino Inclinado com Halteres</h1>
             </section>
             <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
-            </section>
-            <section class="exercicio">
-                <img src="GIF/abdominal.gif" alt="">
-                <h1>Abdominal</h1>
+                <img src="GIF/supino-reto.gif" alt="">
+                <h1>Supino Reto</h1>
             </section>
         </main>
     <?php include_once "footer.php"; ?>
